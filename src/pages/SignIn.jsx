2343a5e0f058a8f6af428158debf2b89df8eaed0
tessavermeulen07 <tessavermeulen@me.com>
2026-01-21 15:20:53 +0100

    bonusopdracht 1 klaar, denk nog even eraan om van de inputlabels componenten te maken.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,25 +1,49 @@
-import React, {useContext} from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext, useState} from 'react';
+import {Link} from 'react-router-dom';
 import {AuthContext} from "../context/AuthContext";
 
 function SignIn() {
     const {isAuth, login} = useContext(AuthContext);
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
 
-  return (
-    <>
-      <h1>Inloggen</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
+                molestias qui quo unde?</p>
 
-      <form onSubmit={login}>
-        <p>*invoervelden*</p>
-        <button
-        type="submit"
-        >Inloggen</button>
-      </form>
+            <form onSubmit={login}>
+                <label htmlFor="email">
+                    <p>E-mailadres:</p>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={emailValue}
+                        onChange={(e) => setEmailValue(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="password">
+                    <p>Wachtwoord:</p>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={passwordValue}
+                        onChange={(e) => setPasswordValue(e.target.value)}
+                    />
+                </label>
 
-      <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
-    </>
-  );
+                <button
+                    type="submit"
+                >Inloggen
+                </button>
+            </form>
+
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
 }
 
 export default SignIn;
