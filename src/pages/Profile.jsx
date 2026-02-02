aa4ad2fcd3261f795220e6b395a6edf426c552c9
tessavermeulen07 <tessavermeulen@me.com>
2026-02-02 15:28:29 +0100

    stappenplan stap 24 gedaan. Al in Swagger een 403 melding bij het uitvoeren van de api/secrets. Dit komt dus ook op de profiel pagina terecht.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,35 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import {AuthContext} from "../context/AuthContext";
+import axios from "axios";
 
 
 
 function Profile() {
 
   const {user, isAuth} = useContext(AuthContext);
+  const [privateContent, setPrivateContent] = useState(null);
 
+  useEffect(() => {
+    const token = localStorage.getItem('JWT');
 
+    async function fetchPrivateContent(){
+     try {
+       const response = await axios.get(`https://novi-backend-api-wgsgz.ondigitalocean.app/api/secrets`, {
+         headers: {
+           'novi-education-project-id': '268aff3c-ae58-411a-a55f-e0c1ec05146d'
+         }
+       });
+       console.log(response);
+       setPrivateContent(response.data);
+     } catch (e) {
+       console.error ('Fout bij het ophalen van private content', e);
+     }
+    }
+    if (token) {
+      void fetchPrivateContent();
+    }
+  }, []);
 
     return (
     <>
@@ -18,10 +39,14 @@ function Profile() {
         <p><strong>Gebruikersnaam:</strong> user.username - deze kan ik niet ophalen, staat niet in de swagger genoteerd.</p>
         <p><strong>Email:</strong> {user.email} </p>
       </section>
-      <section>
-        <h2>Strikt geheime profiel-content</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
-      </section>
+      {privateContent ? (
+          <section>
+            <h2>Afgeschermde content voor ingelogde gebruikers</h2>
+            <p>{privateContent}</p>
+          </section>
+      ) : (
+          <p>Private content kan niet geladen worden.</p>
+      )}
       <p>Terug naar de <Link to="/">Homepagina</Link></p>
     </>
   );
